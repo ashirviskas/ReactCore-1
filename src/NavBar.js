d301f3fb033b1dec8c8e074e0ae7ab3ed721645a
arnas <gecas.arnas@gmail.com>
2017-03-18 10:43:32 +0200

    Added navbar component.

---
+++ src/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import css from './main.css';
+
+class NavBar extends React.Component {
+  render() {
+    return(
+      <div className="header clearfix">
+        <nav>
+          <ul className="nav nav-pills pull-right">
+            <li role="presentation" className="active"><a href="#">Home</a></li>
+            <li role="presentation"><a href="#">About</a></li>
+            <li role="presentation"><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+        <h3 className="text-muted">PCMR</h3>
+      </div>
+  );
+  }
+}
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import NavBar from './NavBar'
 
 ReactDOM.render(
-  <App />,
+  <NavBar />,
   document.getElementById('root')
 );
