8a5b11b738c09c06942dad315bf2ed07bf90eafc
Arnas Gecas <gecas.arnas@gmail.com>
2017-04-24 00:27:52 +0300

    Addedd cpu and settings models.

---
+++ ReactCore/Model/Cpu.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ReactCore.Model
+{
+    public class Cpu
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("url")]
+        public string Url { get; set; } = string.Empty;
+
+        [BsonElement("Procesoriaus branduolių skaičius")]
+        public int CoresCount { get; set; } = 0;
+
+        [BsonElement("price")]
+        public string Price { get; set; } = string.Empty;
+
+        [BsonElement("Procesoriaus lizdo tipas")]
+        public string SocketType { get; set; }
+
+        [BsonElement("model")]
+        public string Model { get; set; }
+
+        [BsonElement("Procesoriaus tipas")]
+        public string Type { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("Maksimalus TDP")]
+        public string Tdp { get; set; }
+
+        [BsonElement("Pridėtas ventiliatorius")]
+        public string HasCooler { get; set; }
+
+        [BsonElement("Procesoriaus  taktavimo dažnis")]
+        public string GhSpeed { get; set; }
+
+        [BsonElement("contents")]
+        public string Contents { get; set; }
+
+        [BsonElement("Integruota grafinė sistema")]
+        public string IntegratedGpu { get; set; } 
+    }
+}
