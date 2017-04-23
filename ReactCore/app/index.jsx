dcfb372dabf6b0687750c79500ae2ea110dba777
Arnas Gecas <gecas.arnas@gmail.com>
2017-04-23 21:20:25 +0300

    Included wizard forms to the application.

--- ReactCore/app/index.jsx
+++ ReactCore/app/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import {
   BrowserRouter as Router,
   Route,
@@ -8,17 +9,23 @@ import Container from './components/Container';
 import App from './components/App';
 import About from './components/About';
 import Repository from './components/Repository';
+import WizardForm from './components/WizardForm';
+
+import store from './store';
 
 import './styles/main.css';
 import './styles/App.css';
 
 ReactDOM.render(
-  <Router>
-    <Container>
-      <Route exact path="/" component={App} />
-      <Route path="/about" component={About} />
-      <Route path="/repository" component={Repository} />
-    </Container>
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <Container>
+        <Route exact path="/" component={App} />
+        <Route path="/about" component={About} />
+        <Route path="/repository" component={Repository} />
+        <Route path="/start" component={WizardForm} />
+      </Container>
+    </Router>
+  </Provider>,
   document.getElementById('root'),
 );
