59d4b10f4468281960c38d87935b605741949e2f
Arnas Gecas <gecas.arnas@gmail.com>
2017-04-24 00:28:35 +0300

    Added mongo driver db context.

---
+++ ReactCore/Data/DbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using ReactCore.Model;
+
+namespace ReactCore.Data
+{
+    public class DbContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public DbContext(IOptions<Settings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            _database = client.GetDatabase(settings.Value.Database);
+        }
+
+        public IMongoCollection<Cpu> Notes => _database.GetCollection<Cpu>("cpu");
+    }
+}
