fa512e48c1dd8949fcfaa918fa681a7947440f47
Artūras <arturas.ciuplys@gmail.com>
2017-04-24 00:07:35 +0300

    updated about us text

--- ReactCore/app/components/About.jsx
+++ ReactCore/app/components/About.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const About = () => (
-  <div>Mes esame trijų studėntų komanda siekianti</div>
+  <div>Mes esame trijų studėntų komanda siekianti sukurti paprastą ir patogią platformą,
+  kurioje būtų galima nesudėtingai susirinkti personalinį kompiuterį.</div>
 
 );
 
