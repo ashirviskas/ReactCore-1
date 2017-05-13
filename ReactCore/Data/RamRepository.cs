13a2c65339dce0c051fe4f690a765fbf885aa406
ashirviskas <minematas@gmail.com>
2017-05-13 16:52:22 +0300

    Correct Ram repository

---
+++ ReactCore/Data/RamRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using ReactCore.Interfaces;
+using ReactCore.Model;
+
+namespace ReactCore.Data
+{
+    public class RamRepository : IRamRepository
+    {
+        private readonly DbContext _context = null;
+
+        public RamRepository(IOptions<Settings> settings)
+        {
+            _context = new DbContext(settings);
+        }
+
+        public async Task<IEnumerable<Ram>> GetAllRams()
+        {
+            try
+            {
+                return await _context.Notes.Find(_ => true).ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                // log or manage the exception
+                throw ex;
+            }
+        }
+
+        public async Task<Ram> GetRam(string id)
+        {
+            var filter = Builders<Ram>.Filter.Eq("Id", id);
+
+            try
+            {
+                return await _context.Notes
+                                .Find(filter)
+                                .FirstOrDefaultAsync();
+            }
+            catch (Exception ex)
+            {
+                // log or manage the exception
+                throw ex;
+            }
+        }
+
+
+
+}
+}
